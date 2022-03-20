46e590f2c517652d6b9ff443335a82f0163fa2ed
Gary Evans <gary@gr4vy.com>
2022-03-20 19:18:54 +0000

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# react-axios-http-jwt
+
+Transmit and refresh JWT authentication tokens without local storage.
+
+Provides a react hook which wraps [axios-http-jwt] to manage JWT authentication.
---
+++ src/index.ts
@@ -0,0 +1,28 @@
+import { AxiosRequestConfig } from 'axios'
+import { useMemo, useState } from 'react'
+import { Client, Authenticated } from 'axios-http-jwt'
+
+const useClient = (
+    config: AxiosRequestConfig,
+    onLogin: (data: any) => Promise<string>,
+    onLogout: () => Promise<void>,
+    onRefresh: () => Promise<string>,
+    header = 'Authorization',
+    headerFormat = (value: string) => `Bearer ${value}`
+) => {
+    const [isAuthenticated, setIsAuthenticated] = useState<Authenticated>(undefined)
+
+    const client = useMemo(() => {
+        return new Client(config, onLogin, onLogout, onRefresh, setIsAuthenticated, header, headerFormat)
+    }, [])
+
+    return {
+        axios: client.axios,
+        isLoading: isAuthenticated === undefined,
+        isAuthenticated,
+        login: client.login,
+        logout: client.logout,
+    }
+}
+
+export default useClient
